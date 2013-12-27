e4522e8d4100c35745315b9519acd7d8258b985b
cis <cis@CISM456.(none)>
2013-12-27 15:13:04 +0530

    added market place application

---
+++ test.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+  <meta charset="UTF-8">
+
+  <title>CodePen - Pen</title>
+
+  <link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
+  <link href="css/bootstrap.css" rel="stylesheet">
+  <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css">
+  
+  <script src="js/jquery-1.7.2.min.js" type="text/javascript"></script>
+  <script src="js/bootstrap.js" type="text/javascript"></script>
+  <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+</head>
+
+<body>
+	<form>
+	<table class="table table-bordered table-striped table-hover">
+			<tr>
+				<td>
+                    <input type="text">
+				</td>
+			</tr>
+			<tr>
+				<td>
+                    <input type="text">
+				</td>
+			</tr>
+	</table>
+</form>
+</body>
